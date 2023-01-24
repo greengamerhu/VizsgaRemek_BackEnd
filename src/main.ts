0436d50c293345e7c35183486bf5612015a349fa
Gabor Halasz <gabor.halasz@level14.hu>
2023-01-24 09:53:10 +0100

    CORS options

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,14 @@ async function bootstrap() {
      alongside with src. You can put them wherever you want, 
      just use the correct path if you use another folder.
   */
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
