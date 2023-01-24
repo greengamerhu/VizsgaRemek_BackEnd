fc7134265660a71739b094bf236a66eb7ba98be4
Gabor Halasz <gabor.halasz@level14.hu>
2023-01-24 13:35:45 +0100

    added typeorm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,13 @@
-import {
-  Controller,
-  Get,
-  Render,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
-import db from './db';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dataSource: DataSource,
+  ) {}
 
   @Get()
   @Render('index')
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      database: 'database',
+      entities: [
+        /* List of entities here */
+      ],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
