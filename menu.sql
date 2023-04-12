9f0828bfc2f41af9fb474f0cd8213bf1439eec2c
Rimoczi Daniel <rimoczidani007@gmail.com>
2023-04-12 11:52:07 +0200

    menu with imagesdump added, env.example added

---
+++ menu.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO `menu` (`food_id`, `food_name`, `food_description`, `food_category`, `food_price`, `food_image`) VALUES
+(1, 'Classic Hamburger', 'A juicy beef patty with lettuce, tomato, onion, and cheese served on a sesame seed bun.', 'Burger', 3900, 'Classic_Hamburger.jpg'),
+(2, 'Bacon Cheeseburger', 'Classic hamburger with crispy bacon and melted cheese.', 'Burger', 4500, 'sajtburger.jpg'),
+(3, 'Mushroom Swiss Burger', 'Classic hamburger with sautéed mushrooms and melted Swiss cheese.', 'Burger', 4900, 'egészséges.jpg'),
+(4, 'Veggie Burger', 'A vegetarian burger made with a patty of mixed vegetables and tofu.', 'Vegetarian', 3900, 'különleges.jpg'),
+(5, 'Double Cheeseburger', 'Two juicy beef patties with lettuce, tomato, onion, and double cheese served on a sesame seed bun.', 'Burger', 5900, 'duppla_burger.jpg'),
+(6, 'BBQ Bacon Burger', 'Classic hamburger with crispy bacon, melted cheddar cheese, and tangy BBQ sauce.', 'Burger', 5200, 'pulled_pork.jpg'),
+(7, 'Jalapeño Burger', 'Classic hamburger with sliced jalapeños, melted pepper jack cheese, and chipotle mayo.', 'Burger', 5100, 'tripla_buger.jpg'),
+(8, 'Chicken Burger', 'Grilled chicken breast with lettuce, tomato, onion, and cheese served on a sesame seed bun.', 'Burger', 4200, 'páros menü.jpg'),
+(9, 'Black Bean Burger', 'A vegetarian burger made with a patty of black beans, corn, and spices.', 'Vegetarian', 3900, 'Hamburger_krumplival.jpg'),
+(10, 'Avocado Burger', 'Classic hamburger with sliced avocado, lettuce, tomato, and garlic mayo.', 'Burger', 5400, 'avokádós.jpg');
