7ae07e5547bf3275f6940684c32a798c51f478cc
Rimóczi Dániel <rimoczidani007@gmail.com>
2023-04-19 17:27:02 +0200

    Api documentattion started, some of dto-s been delete because of usage

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body,  Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UpdateUserDto } from './dto/update-user.dto';
 import RegisterUserDto from './dto/create-user.dto';
 import { AuthGuard } from '@nestjs/passport/dist';
 import {  Roles } from 'src/roles/roles.decorator';
 import { Role } from 'src/roles/role.enum';
 import { RolesGuard } from 'src/roles/roles.guard';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 
 @Controller('user')
@@ -13,6 +13,11 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/register')
+  @ApiOperation({ description: 'Létrehoz egy új usert' })
+  @ApiParam({
+    "name" : "createUserDto",
+    description : "Lásd a Dto szekciónál"
+  })
   create(@Body() createUserDto: RegisterUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -20,20 +25,20 @@ export class UsersController {
   @Get('getall')
   @UseGuards(AuthGuard('bearer'), RolesGuard)
   @Roles(Role.Admin)
-  findAll(@Request() req) {
-    console.log(req.user.role)
+  @ApiOperation({ description: 'Kizárólag adminoknak visszadja a usereket' })
+  findAll() {
     return this.usersService.findAll();
   }
 
   @UseGuards(AuthGuard('bearer'))
   @Get('/profile')
+  @ApiOperation({ description: 'Visszaadja annak a usernek az adatait, amelyik lekérdezi ezt a végpontot' })
+  @ApiParam({
+    "name" : "req",
+    description : "Lásd a Dto szekciónál"
+  })
   findOne(@Request() req) {
     return this.usersService.findOne(req.user);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
