2bbd70cbfeac7c4f79a8885c53b968a5d41f5ae6
Rimoczi Daniel <rimoczidani007@gmail.com>
2023-04-20 13:05:05 +0200

    Docs

--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty } from "class-validator"
 import { Menu } from "src/menu/entities/menu.entity"
 
 export class CreateCartDto {
-    //szoszi
+
     @IsNotEmpty()
+    @ApiProperty({
+      description : "Az adott étel darabszáma",
+      type : 'number',
+      example : 2, 
+      required : true     
+  })
     quantity: number
     @IsNotEmpty()
+    @ApiProperty({
+      description : "A kosárba helyezendő étel",
+      type : 'Menu',
+    example : {/**TODO meg irni a menuitemet */}, 
+      required : true     
+  })
     menuItem : Menu
 }
--- src/cart/dto/update-cart.dto.ts
+++ src/cart/dto/update-cart.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateCartDto } from './create-cart.dto';
 
-export class UpdateCartDto extends PartialType(CreateCartDto) {}
+export class UpdateCartDto extends PartialType(CreateCartDto) {
+}
